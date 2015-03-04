fc2fe61d3bdb72c3b24b6d9dd207d175f1acea5e
Devin Perry <DevinePeople@users.noreply.github.com>
2015-03-04 10:33:19 -0800

    Update Blink.ino

--- arduino/Blink.ino
+++ arduino/Blink.ino
@@ -1,6 +1,6 @@
 
 int led = 13;
-
+//into pin 13 causes light to blink
 void setup() {                
 
   pinMode(led, OUTPUT);     
@@ -9,7 +9,9 @@ void setup() {
 
 void loop() {
   digitalWrite(led, HIGH);   
-  delay(1000);              
-  digitalWrite(led, LOW);    
+  delay(1000);  
+  //increases amount of voltage going to light and the amount of time the voltage stays
+  digitalWrite(led, LOW);
+  //decreases amount of voltage going to light and the amount of time the voltage stays
   delay(1000);              
 }
